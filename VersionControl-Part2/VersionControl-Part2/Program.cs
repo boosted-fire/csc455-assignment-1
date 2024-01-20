63ed61a6e9c7a67997a72c75ea4761984596c3df
Eric Bjerke <ebjerke1@hotmail.com>
2024-01-19 19:52:56 -0600

    Create console menu and skeleton methods

--- VersionControl-Part2/VersionControl-Part2/Program.cs
+++ VersionControl-Part2/VersionControl-Part2/Program.cs
@@ -10,7 +10,68 @@ namespace VersionControl_Part2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
+            var active = true;
+            while (active)
+            {
+                PrintMenu();
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        RandomInteger();
+                        break;
+                    case "2":
+                        TodaysDate();
+                        break;
+                    case "3":
+                        RandomDinosaur();
+                        break;
+                    case "4":
+                        RandomStringMod();
+                        break;
+                    case "q":
+                    case "Q":
+                        active = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid entry, please try again");
+                        break;
+                }
+            }
+        }
+
+        static void PrintMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("1. Random Integer 1-10");
+            Console.WriteLine("2. Today's Date");
+            Console.WriteLine("3. Display Random Dinosaur");
+            Console.WriteLine("4. Random String Modification");
+            Console.WriteLine("Enter q to Quit");
+            Console.WriteLine("-------------------------");
+        }
+
+        static void RandomInteger()
+        {
+
+        }
+
+        static void TodaysDate()
+        {
+
+        }
+
+        static void RandomDinosaur()
+        {
+
+        }
+
+        static void RandomStringMod()
+        {
+
         }
     }
 }
